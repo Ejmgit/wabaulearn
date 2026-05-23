65932c2fc7bdad37df8c84ac71952207d5cdf396
Mhando-Z <mhandosz17@gmail.com>
2026-05-23 10:00:40 +0300

    Phase One

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col ">
-      <h1 className="text-white font-Playfair text-8xl">Wabau Learn</h1>
+      <h1 className="text-white font-Playfair text-3xl">Wabau Learn</h1>
       {/* Navbar section */}
     </div>
   );
